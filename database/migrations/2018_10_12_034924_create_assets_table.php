4f647e1fa1a443191ac2005cd33c1273e371692d
FuadFikri <fuadfikri01@gmail.com>
2018-10-25 20:14:47 +0700

    change data type for desc column

--- database/migrations/2018_10_12_034924_create_assets_table.php
+++ database/migrations/2018_10_12_034924_create_assets_table.php
@@ -16,7 +16,7 @@ class CreateAssetsTable extends Migration
         Schema::create('assets', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('desc');
+            $table->text('desc');
             $table->string('file')->nullable();
             $table->string('thumbnail')->nullable();
             $table->timestamps();
