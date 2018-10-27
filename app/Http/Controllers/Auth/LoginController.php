f8dfff96369fcacaa4a481d53c59ba4a9c918665
FuadFikri <fuadfikri01@gmail.com>
2018-10-27 09:49:55 +0700

    change redirect after login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/data/index';
 
     /**
      * Create a new controller instance.
