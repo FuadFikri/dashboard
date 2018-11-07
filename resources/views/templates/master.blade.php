39b156099a34d2925ed77bed89f128a27cd88920
irsyad <irsyadk4@gmail.com>
2018-11-07 10:47:25 +0700

    Mengubah tampilan jika user masuk sebagai admin atau guest di halaman Documentation

--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -27,7 +27,12 @@
                 <div class="my_text">
                     Welcome
                 </div>
-                <h2>Admin</h2>
+                @if($user = Auth::user())
+                        {{ Auth::user()->name }}
+                @else
+                    Guest
+                @endif
+                <h2></h2>
               </div>
             </div>
             <!-- /menu profile quick info -->
@@ -53,22 +58,33 @@
               <ul class="nav navbar-nav navbar-right">
                 <li class="">
                   <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                    <img src="images/img.jpg" alt="">Admin
+                    <img src="images/img.jpg" alt="">
+                    @if($user = Auth::user())
+                        {{ Auth::user()->name }}
+                    @else
+                        Guest
+                    @endif
                     <span class=" fa fa-angle-down"></span>
                   </a>
                   <ul class="dropdown-menu dropdown-usermenu pull-right">
-                    <li><a href="javascript:;"> Profile</a></li>
-                    <li><a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                                     <i class="fa fa-sign-out pull-right">
-                                                     </i>
-                                                     Log Out
-                        </a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    </li>
+                      @if($user = Auth::user())
+                      <li><a href="javascript:;"> Profile</a></li>
+                      <li><a class="dropdown-item" href="{{ route('logout') }}"
+                                         onclick="event.preventDefault();
+                                                       document.getElementById('logout-form').submit();">
+                                                       <i class="fa fa-sign-out pull-right">
+                                                       </i>
+                                                       Log Out
+                          </a>
+                          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                              @csrf
+                          </form>
+                      </li>
+                      @else
+                      <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                      @endif
                   </ul>
                 </li>
 
